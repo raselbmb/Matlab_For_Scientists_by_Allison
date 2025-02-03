cf4ef7aeabe096155e9718af056f4d7d03a44477
MD RASEL KHAN MANIK <rasel.bmb@gmail.com>
2025-02-03 14:10:36 +0600

    Create Group_Exercise_01.m

---
+++ Group_Exercise_01.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Name: Group_Exercise_01.m
+%   Author: Manik, MRK
+%   Date: 01.03.2025
+%   Details: The SA of a box is calculated based on the given values.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc, clearvars, close all;
+
