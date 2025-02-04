d6429a501a366472bddb40a6f488fdf61441cb3e
MD RASEL KHAN MANIK <rasel.bmb@gmail.com>
2025-02-04 20:41:30 +0600

    Update HW_01_A.m

--- HW_01_A.m
+++ HW_01_A.m
@@ -11,19 +11,19 @@ clc, clearvars, close all;
 
 % Question_1_Part_a
 var11 = 5 + 3;
-fprintf ("var11 = %.f \n", var11);
+fprintf ("Question_1a: var11 = %.f \n", var11);
 
 % Question_1_Part_b
 var12 = 10 - 3;
-fprintf ("var12 = %.f \n", var12);
+fprintf ("Question_1b: var12 = %.f \n", var12);
 
 % Question_1_Part_c
 var13 = 5 * (-3);
-fprintf ("var13 = %.f \n", var13);
+fprintf ("Question_1c: var13 = %.f \n", var13);
 
 % Question_1_Part_d
 var14 = 10^3;
-fprintf ("var14 = %.f \n", var14);
+fprintf ("Question_1d: var14 = %.f \n", var14);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -31,23 +31,23 @@ fprintf ("var14 = %.f \n", var14);
 
 % Question_2_Part_a
 Rooted = sqrt(5);
-fprintf ("Square root = %.4f \n", Rooted);
+fprintf ("Question_2a: Square root = %.4f \n", Rooted);
 
 % Question_2_Part_b
 Rounded_Raised = exp(3);
-fprintf ("Raised to 3rd power = %.4f \n", Rounded_Raised);
+fprintf ("Question_2b: Raised to 3rd power = %.4f \n", Rounded_Raised);
 
 % Question_2_Part_c
 Num = 10.4;
 Round_1 = floor (Num);
 Round_2 = round (Num);
-fprintf ("Question 2c method 1: %.f \n", Round_1);
-fprintf ("Question 2c method 2: %.f \n", Round_2);
+fprintf ("Question_2c_Method_1: %.f \n", Round_1);
+fprintf ("Question_2c_Method_2: %.f \n", Round_2);
 
 % Question_2_Part_d
 Degree_Angle = 46;
 Rounded_Sin_Value = floor (sin(deg2rad(Degree_Angle)));
-fprintf ("Rounded sin(46) to the nearest integer: %d \n", Rounded_Sin_Value);
+fprintf ("Question_2d: Rounded sin(46) to the nearest integer: %d \n", Rounded_Sin_Value);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -58,7 +58,7 @@ fprintf ("Rounded sin(46) to the nearest integer: %d \n", Rounded_Sin_Value);
 format rational
 Output = 5/16 + 2/7;
 
-Format_Command = "Question 3: format rational";
+Format_Command = "Question_3: format rational";
 disp(Format_Command);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -67,12 +67,12 @@ disp(Format_Command);
 
 % Question_4_Part_a
 HorsePounds = 1000;
-fprintf ("The average weight of a horse is %.f lbs. \n", HorsePounds);
+fprintf ("Question_4a: The average weight of a horse is %.f lbs. \n", HorsePounds);
 
 % Question_4_Part_b
 ConversionFactor = 2.2;
 HorseKilos = HorsePounds / ConversionFactor;
-fprintf ("The average weight of a horse is %.f Kg. \n", HorseKilos);
+fprintf ("Question_4b: The average weight of a horse is %.f Kg. \n", HorseKilos);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -85,7 +85,7 @@ Unit = 'g/mol';
 
 Molar_Mass = (MW_C * 6 + MW_H * 10 + MW_O * 5); % Calculation
 
-fprintf ("Molar Mass of starch is: %f %s.\n", Molar_Mass, Unit);
+fprintf ("Question_5: Molar Mass of starch is: %f %s.\n", Molar_Mass, Unit);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -93,7 +93,7 @@ fprintf ("Molar Mass of starch is: %f %s.\n", Molar_Mass, Unit);
 
 ftemp = 108.6;
 ctemp = (ftemp - 32) * 5/9;
-fprintf ("%.2f degrees Fahrenheit equals %.2f degrees Celcius.\n", ftemp, ctemp);
+fprintf ("Question_6: %.2f degrees Fahrenheit equals %.2f degrees Celcius.\n", ftemp, ctemp);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -114,6 +114,6 @@ Numeric_Variable_2 = Numeric_Variable_1 + Increment;
 Numeric_Variable_3 = Numeric_Variable_2 + Increment;
 Numeric_Variable_4 = Numeric_Variable_3 + Increment;
 
-fprintf ("The final value after incrementing: %d \n", Numeric_Variable_4);
+fprintf ("Question_8: The final value after incrementing: %d \n", Numeric_Variable_4);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
