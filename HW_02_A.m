0c504a586e435da51b33325be3780a7ff799f849
MD RASEL KHAN MANIK <rasel.bmb@gmail.com>
2025-02-07 01:12:41 +0600

    Update HW_02_A.m

--- HW_02_A.m
+++ HW_02_A.m
@@ -11,23 +11,23 @@ clc, clearvars, close all;
 
 % Question_1_Part_a
 q1a = rand();
-disp("Question_1a: Random real number using rand, q1a = rand;");
+fprintf("Question_1a: Random real number using rand, q1a = rand;.\n");
 
 % Question_1_Part_b
 q1b = 20 + (50-20) * rand();
-disp("Question_1b: Random number between 20 and 50 using rand, q1b = 20 + (50-20) * rand();");
+fprintf("Question_1b: Random number between 20 and 50 using rand, q1b = 20 + (50-20) * rand();.\n");
 
 % Question_1_Part_c
 q1c = randi([0, 10]);
-disp("Question_1c: Integer between 1 and 10 using randi, q1c = randi([0, 10]);");
+fprintf("Question_1c: Integer between 1 and 10 using randi, q1c = randi([0, 10]);.\n");
 
 % Question_1_Part_d
 q1d = 20 * rand(1, 5);
-disp("Question_1d: Real numbes between 0 and 20 using rand, q1d = 20 * rand(1, 5);");
+fprintf("Question_1d: Real numbes between 0 and 20 using rand, q1d = 20 * rand(1, 5);.\n");
 
 % Question_1_Part_e
 q1e = randi([10, 100], 1, 5);
-disp("Question_1e: 1x5 array using randi, q1e = randi([10, 100], 1, 5);");
+fprintf("Question_1e: 1x5 array using randi, q1e = randi([10, 100], 1, 5);.\n");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -35,19 +35,19 @@ disp("Question_1e: 1x5 array using randi, q1e = randi([10, 100], 1, 5);");
 
 % Question_2_Part_a
 vec_a = [1, 2, 3, 4, 5];
-disp("Question_2a: Writing manualy, vec_a = [1, 2, 3, 4, 5];");
+fprintf("Question_2a: Writing manualy, vec_a = [1, 2, 3, 4, 5];.\n");
 
 % Question_2_Part_b
 vec_b = 1:5;
-disp("Question_2b: Using colon operator, vec_b = 1:5;");
+fprintf("Question_2b: Using colon operator, vec_b = 1:5;.\n");
 
 % Question_2_Part_c
 vec_c = linspace(1, 5, 5);
-disp("Question_2c: Using linspace, vec_c = linspace(1, 5, 5);");
+fprintf("Question_2c: Using linspace, vec_c = linspace(1, 5, 5);.\n");
 
 % Question_2_Part_d
 vec_d = vec_c';
-disp("Question_2d: Transposing, vec_d = vec_c';");
+fprintf("Question_2d: Transposing, vec_d = vec_c';.\n");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -55,22 +55,22 @@ disp("Question_2d: Transposing, vec_d = vec_c';");
 
 % Question_3_Part_a
 vec1 = 1:0.5:3;
-disp("Question_3a: Row vector Q3a, vec1 = 1:0.5:3;");
+fprintf("Question_3a: Row vector Q3a, vec1 = 1:0.5:3;.\n");
 
 % Question_3_Part_b
 vec2 = 5:-1:1;
-disp("Question_3b: Row vector Q3b, vec2 = 5:-1:1;");
+fprintf("Question_3b: Row vector Q3b, vec2 = 5:-1:1;.\n");
 
 % Question_3_Part_c
 vec3 = 10:-2:0;
-disp("Question_3c: Row vector Q3c, vec3 = 10:-2:0;");
+fprintf("Question_3c: Row vector Q3c, vec3 = 10:-2:0;.\n");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Question_4
 
 %save ('Q4_vectors.mat', 'vec1', 'vec2', 'vec3');
-disp("Question_4: See line 72.");
+fprintf("Question_4: See line 72.\n");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -85,24 +85,24 @@ fprintf("Question_5: The vector has %d row(s) and %d column(s). See line 79.\n",
 
 % Question_6_Part_a
 q6a = [1, 1; 1, 1; 1, 1];
-disp("Question_6a: See line 87.");
+fprintf("Question_6a: See line 87.\n");
 
 % Question_6_Part_b
 q6b = ones(3, 2);
-disp("Question_6b: See line 92.");
+fprintf("Question_6b: See line 92.\n");
 
 % Question_6_Part_c
 q6c_1 = [1, 1];
 q6c_2 = [1, 1];
 q6c_3 = [1, 1];
 q6c = [q6c_1; q6c_2; q6c_3];
-disp("Question_6c: See line 95-98.");
+fprintf("Question_6c: See line 95-98.\n");
 
 % Question_6_Part_d
 q6d_1 = ones(2, 2);
 q6d_2 = ones(1, 2);
 q6d = [q6d_1; q6d_2];
-disp("Question_6d: See line 102-104.");
+fprintf("Question_6d: See line 102-104.\n");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -112,17 +112,17 @@ disp("Question_6d: See line 102-104.");
 mat_10 = randi([10, 10], 3, 3);
 %mat_10 = 10 * ones(3, 3);
 mat_rand = rand(3, 3);
-disp("Question_7a: See line 112-114.");
+fprintf("Question_7a: See line 112-114.\n");
 
 % Question_7_Part_b
 mat_new = abs(mat_10 - mat_rand);
-disp("Question_7b: See line 118-119.");
+fprintf("Question_7b: See line 118-119.\n");
 
 % Question_7_Part_c
 mat_concat = [mat_10, mat_new];
 r_mat_concat = size(mat_concat, 1);
 c_mat_concat = size(mat_concat, 2);
-fprintf ("Question_7c: mat_concat vector has %d row(s) and %d column(s).\n", r_mat_concat, c_mat_concat);
+fprintf("Question_7c: mat_concat vector has %d row(s) and %d column(s).\n", r_mat_concat, c_mat_concat);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -140,7 +140,7 @@ else
     concat_dim = 1;
 end
 
-fprintf ("Question_8a_1: %s along dimension %d.\n", concat_type, concat_dim);
+fprintf("Question_8a_1: %s along dimension %d.\n", concat_type, concat_dim);
 
 % Question_8_Part_a_better
 concat_dim = 2;
@@ -155,12 +155,12 @@ fprintf("Question_8a_2: %s along dimension %d.\n", concat_type, concat_dim);
 
 % Question_8_Part_b
 mat = [mat_3x5, mat_3x2];
-disp ("Question_8b: See line 157.");
+fprintf("Question_8b: See line 157.\n");
 
 % Question_8_Part_c
 [r_mat, c_mat] = size(mat);
 all_ones = ones(r_mat, c_mat);
-fprintf ("Question_8c: The syntax for all ones of the size of matrix mat: " + ...
-    "all_ones = ones(r_mat, c_mat); where [r_mat, c_mat] = size(mat);.")
+fprintf("Question_8c: The syntax for all ones of the size of matrix mat: " + ...
+    "all_ones = ones(r_mat, c_mat); where [r_mat, c_mat] = size(mat);.\n")
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
